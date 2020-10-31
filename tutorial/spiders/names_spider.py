b28fa909aef852719428dd5649933746807a0e7d
inci <inci.boduroglu@gmail.com>
2020-10-31 11:57:35 +0300

    feat: add link follow

--- tutorial/spiders/names_spider.py
+++ tutorial/spiders/names_spider.py
@@ -5,9 +5,17 @@ class NamesSpider(scrapy.Spider):
     name = "names"
 
     start_urls = [
-        "https://www.behindthename.com/names/usage/italian"
+        "https://www.behindthename.com/names/list"
     ]
 
     def parse(self, response):
-        for name in response.css(".listname a::text").getall():
-            print(name)
+        def parse_page(page_response):
+            yield {
+                'name': page_response.css(".listname a::text").get(),
+                # 'description':
+            }
+
+        usages = response.css(".usagelist a::attr(href)").getall()
+        for usage in usages:
+            print(usage)
+            yield response.follow(usage, callback=parse_page)
