5c5dba2ecd1dd0382346503520874e795e5b785f
inci <inci.boduroglu@gmail.com>
2020-11-09 18:39:18 +0300

    added csv file headers

--- scrappy-doo/spooder_pipeline.py
+++ scrappy-doo/spooder_pipeline.py
@@ -1,17 +1,25 @@
 import csv
+import os.path
 
 
 class SpooderPipeline(object):
 
     def process_item(self, item, spider):
+        field_names = ['name', 'usage', 'gender', 'related-names']
+
         # get category and use it as filename
         page_name = item["page_name"]
         filename = f'{page_name}.csv'
+
+        # check if file has been created before. if yes; create, and add headers
+        if not os.path.isfile(filename):
+            with open(filename, 'w', newline='') as f:
+                writer = csv.DictWriter(f, fieldnames=field_names)
+                writer.writeheader()
+
         del item["page_name"]
 
         with open(filename, 'a', newline='') as f:
-            field_names = ['name', 'usage', 'gender', 'related-names']
-
             writer = csv.DictWriter(f, fieldnames=field_names)
             # writer.writeheader()
             writer.writerow(item)
